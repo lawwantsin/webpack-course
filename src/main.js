43c8406a18cc787f00fe183b9f401f17de9554d6
Lawrence Whiteside <law@cinemasetfree.com>
2017-08-30 12:12:43 -0700

    Ep05 done

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
+require("babel-runtime/regenerator")
 require("./main.css")
 require("./images/link.jpg")
 require("./index.html")
 
-var a = () => {
-  console.log("Hello from the future!")
+var a = async args => {
+  const { a, b } = args
+  await console.log("Hello from the future!", a, b)
 }
+
+a({ a: 1, b: 2 })
