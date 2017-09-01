3239134332d2dc47471f83960fd102c3d776e126
Lawrence Whiteside <law@cinemasetfree.com>
2017-09-01 16:57:56 -0700

    quick fix

---
+++ src/server/express.js
@@ -0,0 +1,32 @@
+import express from "express"
+const server = express()
+import path from "path"
+
+const isProd = process.env.NODE_ENV === "production"
+if (!isProd) {
+  const webpack = require("webpack")
+  const config = require("../../config/webpack.dev.js")
+  const compiler = webpack(config)
+
+  const webpackDevMiddleware = require("webpack-dev-middleware")(
+    compiler,
+    config.devServer
+  )
+
+  const webpackHotMiddlware = require("webpack-hot-middleware")(
+    compiler,
+    config.devServer
+  )
+
+  server.use(webpackDevMiddleware)
+  server.use(webpackHotMiddlware)
+  console.log("Middleware enabled")
+}
+
+const staticMiddleware = express.static("dist")
+server.use(staticMiddleware)
+
+const PORT = 8080
+server.listen(PORT, () => {
+  console.log(`Server listening on http://localhost:${PORT}`)
+})
