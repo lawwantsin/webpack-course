3b8fc0ce347129d0bbc29bd435aee8da5b654629
Lawrence Whiteside <law@cinemasetfree.com>
2017-10-16 17:43:36 -0700

    quick fix

--- src/counter.js
+++ src/counter.js
@@ -14,10 +14,18 @@ export default class extends React.Component {
 
   render() {
     return (
-      <div onClick={this.climb.bind(this)}>
-        <h1 style={{ color: "black" }}>
-          {this.state.count}
-        </h1>
+      <div className="profile">
+        <img src={require("./images/link.jpg")} />
+        <h1>Link's Journal</h1>
+        <div class="content">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </div>
       </div>
     )
   }
--- src/main.js
+++ src/main.js
@@ -1,3 +1,3 @@
 require("./main.css")
 require("./images/link.jpg")
-// require("./app")
+require("./app")
