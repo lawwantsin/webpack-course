6608ce5287283c5cfa9bcc8098d262d5a1e119f8
Lawrence Whiteside <law@cinemasetfree.com>
2017-10-17 23:38:16 -0700

    update

--- src/components/AppRoot.js
+++ src/components/AppRoot.js
@@ -1,5 +1,48 @@
 import React from "react"
 import BioText from "../../data/bio"
+import styled, { injectGlobal } from "styled-components"
+
+injectGlobal`
+  body {
+    margin: 0;
+    background-color: #444;
+    font-family: Helvetica;
+  }
+`
+
+const Profile = styled.div`
+  display: flex;
+  -ms-align-items: center;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  flex-flow: column;
+  img {
+    border-radius: 100%;
+    border: 5px;
+    width: 300px;
+    box-shadow: 0 0 20px black;
+  }
+
+  h1 {
+    font-size: 5em;
+    font-family: sans-serif;
+    color: white;
+    text-shadow: 0 0 20px black;
+    text-align: center;
+  }
+`
+
+const Content = styled.div`
+  background-color: #ffffe0;
+  box-shadow: 0 0 20px black;
+  font-family: sans-serif;
+  line-height: 1.5em;
+  max-width: 720px;
+  margin: 0 auto;
+  padding: 20px;
+  border-radius: 10px;
+`
 
 export default class extends React.Component {
   constructor(props) {
@@ -9,11 +52,11 @@ export default class extends React.Component {
 
   render() {
     return (
-      <div className="profile">
+      <Profile>
         <img src={require("../images/link.jpg")} />
         <h1>Link's Journal</h1>
-        <div className="content">{BioText}</div>
-      </div>
+        <Content>{BioText}</Content>
+      </Profile>
     )
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,5 +1,3 @@
-require("webpack-hot-middleware/client?reload=true")
 require("./main.css")
-require("./content.css")
 require("./images/link.jpg")
 require("./app")
