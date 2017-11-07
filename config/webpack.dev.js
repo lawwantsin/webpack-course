5b29c207c50db5111044db4502059c9aa1d3b2eb
Lawrence Whiteside <law@cinemasetfree.com>
2017-11-07 10:36:49 -0800

    quick fix

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -6,8 +6,7 @@ module.exports = {
   },
   output: {
     filename: "[name]-bundle.js",
-    path: path.resolve(__dirname, "../dist/js"),
-    publicPath: "/js"
+    path: path.resolve(__dirname, "../dist")
   },
   devServer: {
     contentBase: "dist"
