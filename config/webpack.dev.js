17e9a1270b60e752f53ee5547f03556cf2431149
Lawrence Whiteside <law@cinemasetfree.com>
2018-02-26 08:16:48 -0800

    webpack 4

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -4,10 +4,11 @@ module.exports = {
   entry: {
     main: "./src/main.js"
   },
+  mode: "development",
   output: {
     filename: "[name]-bundle.js",
     path: path.resolve(__dirname, "../dist/js"),
-    publicPath: "/js"
+    publicPath: "/"
   },
   devServer: {
     contentBase: "dist"
