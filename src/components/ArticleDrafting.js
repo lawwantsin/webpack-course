f777efe2964b9b5e48b9a0360c918f08540158a7
Lawrence Whiteside <law@cinemasetfree.com>
2018-04-02 12:10:15 -0700

    quick fix

--- src/components/ArticleDrafting.js
+++ src/components/ArticleDrafting.js
@@ -3,26 +3,25 @@ import "../css/Article.css"
 import NotFound from "./NotFound"
 
 export default props => {
-  console.log(props)
-  // try {
-  const MarkdownData = require(`../../data/${props.site}/${
-    props.match.params.id
-  }.md`)
-  const billboardStyle = {
-    backgroundImage: `url(${MarkdownData.posterImage})`
+  try {
+    const MarkdownData = require(`../../data/${props.site}/${
+      props.match.params.id
+    }.md`)
+    const billboardStyle = {
+      backgroundImage: `url(${MarkdownData.posterImage})`
+    }
+    import(`../css/${props.site}/theme.css`)
+    return (
+      <div className="Article">
+        <div className="billboard" style={billboardStyle} />
+        <h1>{MarkdownData.title}</h1>
+        <div
+          className="content"
+          dangerouslySetInnerHTML={{ __html: MarkdownData.__content }}
+        />
+      </div>
+    )
+  } catch (error) {
+    return <NotFound />
   }
-  import(`../css/${props.site}/theme.css`)
-  return (
-    <div className="Article">
-      <div className="billboard" style={billboardStyle} />
-      <h1>{MarkdownData.title}</h1>
-      <div
-        className="content"
-        dangerouslySetInnerHTML={{ __html: MarkdownData.__content }}
-      />
-    </div>
-  )
-  // } catch (error) {
-  //   return error
-  // }
 }
