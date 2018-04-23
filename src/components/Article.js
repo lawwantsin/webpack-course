a90053c93fe31639e7bd162ce5083c7968bf5447
Lawrence Whiteside <law@cinemasetfree.com>
2018-04-22 20:36:56 -0700

    quick fix

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,8 +1,23 @@
 import React from "react"
 import "../css/Article.css"
 
-export default () => (
-  <div>
-    <h1>Article</h1>
-  </div>
-)
+export default props => {
+  const siteConfig = require(`../../data/${props.site}/siteConfig.js`)
+  const imagePath = require(`../images/${siteConfig.aboutImage}`)
+
+  const MarkdownData = require(`../../data/${props.site}/bio.md`)
+  import(`../css/${props.site}/theme.css`)
+
+  return (
+    <div>
+      <div className="profile">
+        <img src={imagePath} />
+        <h1>{MarkdownData.title}</h1>
+        <div
+          className="content"
+          dangerouslySetInnerHTML={{ __html: MarkdownData.__content }}
+        />
+      </div>
+    </div>
+  )
+}
