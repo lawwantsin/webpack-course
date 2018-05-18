7e690b3934fc4e7cccc0db19639057bc7598add6
Lawrence Whiteside <law@cinemasetfree.com>
2018-05-18 11:01:52 -0700

    quick fix

--- src/store.js
+++ src/store.js
@@ -2,8 +2,6 @@ import { createStore, applyMiddleware, compose } from "redux"
 import { fetchArticle } from "./reducers"
 import thunk from "redux-thunk"
 
-const preloadedState = {}
-
 const composeEnhancers =
   typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
     ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
@@ -12,12 +10,5 @@ const composeEnhancers =
 const enhancer = composeEnhancers(applyMiddleware(thunk))
 
 export default initialState => {
-  const store = createStore(fetchArticle, preloadedState, enhancer)
-  if (module.hot) {
-    module.hot.accept("./reducers", () =>
-      store.replaceReducer(require("./reducers"))
-    )
-  }
-
-  return store
+  return createStore(fetchArticle, initialState, enhancer)
 }
