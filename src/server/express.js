f0b4ff15e33ee4a7f9d62ac05152482e5185c839
Lawrence Whiteside <law@cinemasetfree.com>
2018-06-20 13:37:44 -0700

    quick fix

--- src/server/express.js
+++ src/server/express.js
@@ -2,26 +2,23 @@ import express from "express"
 const server = express()
 import path from "path"
 
-const isProd = process.env.NODE_ENV === "production"
-if (!isProd) {
-  const webpack = require("webpack")
-  const config = require("../../config/webpack.dev.js")
-  const compiler = webpack(config)
+const webpack = require("webpack")
+const config = require("../../config/webpack.dev.js")
+const compiler = webpack(config)
 
-  const webpackDevMiddleware = require("webpack-dev-middleware")(
-    compiler,
-    config.devServer
-  )
+const webpackDevMiddleware = require("webpack-dev-middleware")(
+  compiler,
+  config.devServer
+)
 
-  const webpackHotMiddlware = require("webpack-hot-middleware")(
-    compiler,
-    config.devServer
-  )
+const webpackHotMiddlware = require("webpack-hot-middleware")(
+  compiler,
+  config.devServer
+)
 
-  server.use(webpackDevMiddleware)
-  server.use(webpackHotMiddlware)
-  console.log("Middleware enabled")
-}
+server.use(webpackDevMiddleware)
+server.use(webpackHotMiddlware)
+console.log("Middleware enabled")
 
 const staticMiddleware = express.static("dist")
 server.use(staticMiddleware)
