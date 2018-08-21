cb92146d3813ce7258e8cf82dbc36a18683479ab
Lawrence Whiteside <law@cinemasetfree.com>
2018-08-21 07:44:26 -0600

    quick fix

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,6 +1,5 @@
 const path = require("path")
 const webpack = require("webpack")
-const ExtractTextPlugin = require("extract-text-webpack-plugin")
 const HTMLWebpackPlugin = require("html-webpack-plugin")
 const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin")
 const UglifyJSPlugin = require("uglifyjs-webpack-plugin")
